f32ce278c2eef3b209b1fcce8b75d3cabdb082cf
zhangyuchi.inch <zhangyuchi.inch@bytedance.com>
2021-01-22 14:24:53 +0800

    格式化代码

--- bin/index.js
+++ bin/index.js
@@ -8,13 +8,11 @@ const currentNodeVersion = process.versions.node;
 
 if (semver.lt(currentNodeVersion, '10.0.0')) {
     spinner.fail(
-        '你当前node版本为 ' +
-        chalk.red(currentNodeVersion) +
-        '。\n' +
-        '  该项目要求node版本必须 ' +
-        chalk.cyan('>= 10.0.0') +
-        ' 。\n' +
-        '  请升级你的node！'
+        `你当前node版本为 ${chalk.red(currentNodeVersion)
+        }。\n` +
+        `  该项目要求node版本必须 ${chalk.cyan('>= 10.0.0')
+        } 。\n` +
+        `  请升级你的node！`
     );
 
     process.exit(1);
